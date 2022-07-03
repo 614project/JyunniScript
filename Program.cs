676d22890f97d9843006e8050363c2f015252ecf
614 <71429443+614project@users.noreply.github.com>
2022-07-03 20:28:13 +0900

    와샌즈으
    다신안만들레....

---
+++ Program.cs
@@ -0,0 +1,357 @@
+﻿using System;
+using System.IO;
+
+public partial class Program
+{
+    static bool stop = false;
+    static bool error = false;
+    static List<Variable> Variables = new();
+    static List<string> Names = new();
+
+    static void Main(string[] cmd)
+    {
+        if (cmd.Length == 0)
+        {
+            Inter();
+            return;
+        }
+        if (!File.Exists(cmd[0]))
+        {
+            Console.Write("존재하지 않는 파일 경로입니다.");
+            return;
+        }
+        Scripter(File.ReadAllLines(cmd[0]));
+        Console.ReadKey();
+    }
+
+    static void Scripter(string[] text)
+    {
+        if (text.Length == 0) return;
+        foreach(string dcr in text)
+        {
+            Runner(dcr);
+            if (stop || error) return;
+        }
+    }
+
+    static void Runner(string inputtext)
+    {
+        if ((inputtext = inputtext.Trim()).Length == 0) return;
+        List<string> c = new();
+        string p = inputtext;
+        int rint = 0, k =0;
+        float d=0;
+        while (p.Length != 0)
+        {
+            if ((rint = p.IndexOf(' ')) == -1)
+            {
+                if (p.Length != 0) c.Add(p);
+                break;
+            }
+            c.Add(p.Substring(0, rint));
+            p = p.Substring(rint).TrimStart();
+        }
+        if (c.Count == 0 || c[0].Length == 0) return;
+        bool CheckDuobleDot()
+        {
+            if ((rint = inputtext.IndexOf(':')) == -1)
+            {
+                Console.WriteLine("문자열 시작어 ':' 가 없습니다.");
+                error = true;
+                return true;
+            }
+            return false;
+        }
+        bool CheckLength(int index,string? message = null,bool er = true)
+        {
+            if (c.Count == index || c[index] == "")
+            {
+                if (message != null)Console.WriteLine(message);
+                error = er;
+                return true;
+            }
+            return false;
+        }
+        bool InputVariable(out string outstr)
+        {
+            string str = inputtext;
+            outstr = "Error";
+            str = str.Substring(++rint).Replace("\\n", "\n").Replace("\\.", "\\c").Replace("\\[","\\s").Replace("\\]","\\e");
+            int o = 0,u = 0;
+            List<int> point = new();
+            List<string> value = new();
+            string t;
+            while ((o = str.IndexOf("[")) != -1)
+            {
+                if (
+                    str.Length <= o + 1 ||
+                    (u = (t = str.Substring(o + 1)).IndexOf(']'))
+                    == -1
+                    )
+                {
+                    Console.WriteLine("변수 이름의 끝은 ']' 로 끝나야 합니다.");
+                    error = true;
+                    return true;
+                }
+                if (GetVariable(t.Substring(0,u),out t))
+                {
+                    return true;
+                }
+                point.Add(o);
+                value.Add(t);
+                str = str.Remove(o, u + 2);
+            }
+            for (int i = point.Count - 1;i>=0;i--)
+            {
+                str =  str.Insert(point[i], value[i]);
+            }
+            outstr = str.Replace("\\s", "[").Replace("\\e", "]").Replace("\\c","\\");
+            return false;
+        }
+        bool GetVariable(string varname,out string str)
+        {
+            str = "Error";
+            int index;
+            if (varname == "")
+            {
+                Console.WriteLine("'[' 와 ']' 사이 안에 변수 이름을 적어야 합니다.");
+                error = true;
+                return true;
+            }
+            if ((index = Names.IndexOf(varname)) == -1)
+            {
+                Console.WriteLine($"'{varname}' 은 없는 변수명입니다.");
+                error = true;
+                return true;
+            }
+            str = Variables[index].Source.ToString();
+            return false;
+        }
+        if (c[0].StartsWith('['))
+        {
+            if ((rint = c[0].IndexOf(']')) == -1)
+            {
+                Console.WriteLine("변수 이름의 끝은 ']' 로 끝나야 합니다.");
+                error = true;
+                return;
+            }
+            p = c[0].Substring(1, rint - 1);
+            if ((k = Names.IndexOf(p)) == -1)
+            {
+                Console.WriteLine($"'{p}' 는 없는 변수명입니다.");
+                error = true;
+                return;
+            }
+            switch (Variables[k].Type)
+            {
+                case 's':
+                    if (CheckDuobleDot()) return;
+                    if (InputVariable(out p)) return;
+                    Variables[k].Source = p;
+                    break;
+                case 'i':
+                    if (CheckLength(1, "변수에 넣을 값을 입력해야 합니다.")) return;
+                    if (!int.TryParse(c[1], out rint))
+                    {
+                        Console.WriteLine("변수에 넣을 값이 정수(int)가 아닙니다.");
+                        error = true;
+                        return;
+                    }
+                    Variables[k].Source = rint;
+                    break;
+                case 'f':
+                    if (CheckLength(1, "변수에 넣을 값을 입력해야 합니다.")) return;
+                    if (!float.TryParse(c[1], out d))
+                    {
+                        Console.WriteLine("변수에 넣을 값이 정수(int)가 아닙니다.");
+                        error = true;
+                        return;
+                    }
+                    Variables[k].Source = d;
+                    break;
+                default:
+                    Console.WriteLine($"'{p}' 는 잘못된 타입을 가진 변수형입니다.");
+                    error = true;
+                    break;
+            }
+            return;
+        }
+        switch (c[0].ToLower())
+        {
+            case "print":
+            case "write":
+                if (CheckDuobleDot()) return;
+                if (InputVariable(out p)) return;
+                Console.Write(p);
+                return;
+            case "println":
+            case "writeline":
+                if (CheckDuobleDot()) return;
+                if (InputVariable(out p)) return;
+                Console.WriteLine(p);
+                return;
+            case "stop":
+            case "exit":
+                stop = true;
+                return;
+            case "help":
+                if (CheckLength(1))
+                {
+                    Console.Write(const_helplist);
+                }
+                else Helper(c[1]);
+                return;
+            case "clear":
+                Console.Clear();
+                return;
+            case "wait":
+                if (CheckLength(1, "'wait' 뒤에 지연할 숫자를 입력해야합니다.")) return;
+                
+                if (!float.TryParse(c[1],out d))
+                {
+                    Console.WriteLine("입력된 값이 정수 또는 실수가 아닙니다.");
+                    error = true;
+                    return;
+                }
+                if (d < 0)
+                {
+                    Console.WriteLine("입력된 값이 0 미만의 음수입니다.");
+                    error = true;
+                    return;
+                }
+                Thread.Sleep((int)d * 1000);
+                return;
+            case "delay":
+                if (CheckLength(1, "'delay' 뒤에 지연할 숫자를 입력해야합니다.")) return;
+                if (!int.TryParse(c[1], out rint))
+                {
+                    Console.WriteLine("입력된 값이 정수가 아닙니다.");
+                    error = true;
+                    return;
+                }
+                if (rint<0)
+                {
+                    Console.WriteLine("입력된 값이 0 미만의 음수입니다.");
+                    error = true;
+                    return;
+                }
+                Thread.Sleep(rint);
+                return;
+            case "int":
+                if (CheckLength(1, "'int' 뒤에 변수 이름을 적어주세요.")) return;
+                if (c[1].IndexOfAny("!@#$%^&*()+-=~`|\\<>,.?/;:'\"[]{}".ToArray()) != -1)
+                {
+                    Console.WriteLine("'_'(언더바) 를 제외한 모든 특수기호는 변수 이름으로 사용할수 없습니다.");
+                    error = true;
+                    return;
+                }
+                if (Names.IndexOf(c[1]) != -1)
+                {
+                    Console.WriteLine($"'{c[1]}' 의 이름을 가진 변수가 이미 존재합니다.");
+                    error = true;
+                    return;
+                }
+                if (CheckLength(2,null,false))
+                {
+                    Variables.Add(new('i', 0));
+                    Names.Add(c[1]);
+                } else
+                {
+                    if (!int.TryParse(c[2],out rint))
+                    {
+                        Console.WriteLine("변수에 넣을 값이 정수(int)가 아닙니다.");
+                        error = true;
+                        return;
+                    }
+                    Variables.Add(new('i', rint));
+                    Names.Add(c[1]);
+                }
+                return;
+            case "string":
+                if (CheckLength(1, "'string' 뒤에 변수 이름을 적어주세요.")) return;
+                if (c[1].IndexOfAny("!@#$%^&*()+-=~`|\\<>,.?/;:'\"[]{}".ToArray()) != -1)
+                {
+                    Console.WriteLine("'_'(언더바) 를 제외한 모든 특수기호는 변수 이름으로 사용할수 없습니다.");
+                    error = true;
+                    return;
+                }
+                if (Names.IndexOf(c[1]) != -1)
+                {
+                    Console.WriteLine($"'{c[1]}' 의 이름을 가진 변수가 이미 존재합니다.");
+                    error = true;
+                    return;
+                }
+                if (CheckLength(2, null, false))
+                {
+                    Variables.Add(new('s', string.Empty));
+                    Names.Add(c[1]);
+                }
+                else
+                {
+                    if (CheckDuobleDot()) return;
+                    if (InputVariable(out p)) return;
+                    Variables.Add(new('s', p));
+                    Names.Add(c[1]);
+                }
+                return;
+            case "read":
+                if (CheckLength(1, "입력을 받을 변수명을 입력하세요.")) return;
+                if ((k = Names.IndexOf(c[1])) == -1)
+                {
+                    Console.WriteLine($"'{c[1]}' 는 없는 변수명입니다.");
+                    error = true;
+                    return;
+                }
+                if (Variables[k].Type != 's')
+                {
+                    Console.WriteLine("문자열(string) 변수만 입력을 받을수 있습니다.");
+                    error = true;
+                    return;
+                }
+                Variables[k].Source = Console.ReadLine();
+                break;
+            case "free":
+                if (CheckLength(1, "제거할 변수명을 입력하세요.")) return;
+                if ((k = Names.IndexOf(c[1])) == -1)
+                {
+                    Console.WriteLine($"'{c[1]}' 는 없는 변수명입니다.");
+                    error = true;
+                    return;
+                }
+                Names.RemoveAt(k);
+                Variables.RemoveAt(k);
+                break;
+
+            default:
+                Console.WriteLine($"'{c[0]}'은 알수없는 명령어입니다.");
+                error = true;
+                return;
+        }
+    }
+
+    static void Inter()
+    {
+        Console.WriteLine("Jyunni Script [" + const_version + "]\n(c) 614Project. All rights reserved.\n'help' 를 입력해 명령어 목록을 확인해보세요.\n'stop' 또는 'exit' 를 입력해 스크립터를 종료할수 있습니다.");
+        do
+        {
+            Console.Write("\n>>>");
+            Runner(Console.ReadLine());
+            if (stop) return;
+        } while (true);
+    }
+}
+
+class Variable
+{
+    public char Type = 'B';
+    public object Source = false;
+    public Variable()
+    {
+
+    }
+    public Variable(char type, object source)
+    {
+        Type = type;
+        Source = source;
+    }
+}
---
+++ data.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+public partial class Program
+{
+    const string const_version = "0.1.4";
+
+    const string const_help_print = "'print :[string]' 또는 'write :[string]' : 원하는 문자열을 출력합니다.";
+
+    const string const_help_println = "'println :[string]' 또는 'writeline :[string]' : 원하는 문자열을 한 줄에 출력합니다.";
+
+    const string const_help_exit = "'stop' 또는 'exit' : Jyunni Script를 종료합니다.";
+
+    const string const_help_help = "'help' [command] : 원하는 명령어에 대한 설명을 출력합니다.";
+
+    const string const_help_clear = "'clear' : 현재 콘솔에 출력된 내용을 모두 지웁니다.";
+
+    const string const_help_wait = "'wait' [float]' : 원하는 값만큼 실행을 지연합니다. 기준은 '1초' 입니다. ('6.14'을 입력하면 6.14초가 지연됩니다.)";
+
+    const string const_help_delay = "'delay [int]' : 원하는 값만큼 실행을 지연합니다. 기준은 '0.001'초 입니다. ('614'를 입력하면 0.614초가 지연됩니다.)";
+
+    const string const_help_int = "'int [string]' 또는 'int [string] [int]' : 새 정수형 변수를 원하는 이름으로 생성합니다. 또는 원하는 값도 넣습니다. (이름은 언더바 '_' 를 제외하고는 특수문자를 쓸수 없습니다.)";
+    
+    const string const_help_string = "'string [string]' 또는 'string [string] [string]' : 새 문자열 변수를 원하는 이름으로 생성합니다. 또는 원하는 값도 넣습니다. (이름은 언더바 '_' 를 제외하고는 특수문자를 쓸수 없습니다.)";
+
+    const string const_help_read = "'read [문자열 변수의 이름]' : 콘솔에서 입력을 받아 원하는 변수에 저장합니다.";
+
+    const string const_help_free = "'free' [변수의 이름]' : 원하는 변수를 제거합니다.";
+
+    const string const_helplist = $"\n명령어 목록:\n{const_help_print}\n{const_help_println}\n{const_help_clear}\n{const_help_int}\n{const_help_string}\n{const_help_read}\n{const_help_free}\n{const_help_wait}\n{const_help_delay}\n{const_help_exit}\n{const_help_help}\n";
+
+    static void Helper(string cmd)
+    {
+        switch (cmd) {
+            case "print":
+            case "write":
+                Console.WriteLine(const_help_print);
+                break;
+            case "println":
+            case "writeline":
+                Console.WriteLine(const_help_println);
+                break;
+            case "exit":
+            case "stop":
+                Console.WriteLine(const_help_exit);
+                break;
+            case "help":
+                Console.WriteLine(const_help_help);
+                break;
+            case "clear":
+                Console.WriteLine(const_help_clear);
+                break;
+            case "wait":
+                Console.WriteLine(const_help_wait);
+                break;
+            case "delay":
+                Console.WriteLine(const_help_delay);
+                break;
+            case "int":
+                Console.WriteLine(const_help_int);
+                break;
+            case "string":
+                Console.WriteLine(const_help_string);
+                break;
+            case "read":
+                Console.WriteLine(const_help_read);
+                break;
+            case "free":
+                Console.WriteLine(const_help_free);
+                break;
+            default:
+                Console.WriteLine($"help: '{cmd}' 는 알수없는 명령어입니다.");
+                error = true;
+                break;
+        }
+    }
+}
